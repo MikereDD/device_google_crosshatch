e935690ba8b9482fd318c87cdef6a9c2edc359ca
Christian Oder <myself5@carbonrom.org>
2018-10-22 13:09:56 -0400

    Don't enforce priv app whitelist
    Change-Id: Ib3cb798ace361a3565536148346533f314e373c3

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=disable
 
 # Enable on-access verification of priv apps. This requires fs-verity support in kernel.
 PRODUCT_PRODUCT_PROPERTIES += \
